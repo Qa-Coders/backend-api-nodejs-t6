f71b449ae79efe3275abcbef312c132f45d1fe72
wacoders <w.silva@qacoders-treinamento.com>
2022-05-11 20:01:55 -0300

    express configuration file server.js

---
+++ config/routes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+module.exports = function (server) {
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
+
+    server.use('/status', (req, res) =>
+        res.send(`BACKEND is runner.`));
+
+    server.use(express.static(require('path').join(__dirname, '../public')));
+}
