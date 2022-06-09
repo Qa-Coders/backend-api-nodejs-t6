ae6ffd8592973a8e90e3f6b8bb40f954c1e835ed
Fabio Martins <fabio.rocha.martins@hotmail.com>
2022-06-09 19:35:43 -0300

    express configuration file server.js

---
+++ config/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+module.exports = function(server) {
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
+
+    server.use('/status', (req, res) => 
+        res.send(`BACKEND is runner.`));
+    
+    const register = require('../api/registerService');
+    register.register(protectedApi, '/register')   
+        
+    server.use(express.static(require('path').join(__dirname, '../public')));
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-Projeto Backend-API
-
+Projeto backend-API.
+Projeto do backend da turma t6
+OBS: Todo push deve ser feito através de uma branch
